65637e037ab3b3bcad4101ec62ccd51c61b08d23
Unknown <afgarcia0479@misena.edu.co>
2017-07-25 23:10:02 -0500

    Inicial Commit Actividad 01

---
+++ 2017-07-18 Java_Actividad01/src/ejemplo/Ejemplo.java
@@ -0,0 +1,110 @@
+/*
+ * Explicación
+ */
+package ejemplo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Andres Felipe Garcia Ramirez
+ */
+public class Ejemplo {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	@SuppressWarnings("empty-statement")
+	public static void main(String[] args) {
+		// TODO code application logic here
+		String id;
+		String identidad;
+		String nombre;
+		int edad;
+		float estatura;
+		float salarioHora;
+		char genero = 0;
+		int rep;
+		int[] horasLaboralxSemana = new int[4];
+		float valorTotal;
+		int horasTotal;
+		int antiguedad;
+		float bonificacion;
+
+		/*
+		//Ingresa datos básicos
+		do {
+			identidad = JOptionPane.showInputDialog("Por favor escriba su número de identidad");
+			nombre = JOptionPane.showInputDialog("Digite su nombre");
+			edad = Integer.parseInt(JOptionPane.showInputDialog("Digite su edad"));
+			estatura = Float.parseFloat(JOptionPane.showInputDialog("Digite su estatura"));
+			//salario = Float.parseFloat(JOptionPane.showInputDialog("Digite su salario"));
+			genero = (JOptionPane.showInputDialog(
+					"Digite su genero, si es másculino escriba M, sino escriba F.")).charAt(0);
+			rep = Integer.parseInt(JOptionPane.showInputDialog("¿Desea continuar? Si marca 1, No marca 2"));
+		} while (rep == 1);
+		 */
+
+ /*  
+        int paso1 = 1;
+        while (paso1 == 1) {
+            paso1 = 0;
+        }
+		 */
+ /*
+		//Ingresa cantidad horas laborales por cada semana
+		horasTotal = 0;
+		int MAX_HORASLABOR = 40;
+		for (int i = 0; i < horasLaboralxSemana.length; i++) {
+			do {
+				horasLaboralxSemana[i] = Integer.parseInt(
+						JOptionPane.showInputDialog(
+								"Por favor escriba cantidad horas laboral en " + (i + 1) + "° Semana"));
+				if (horasLaboralxSemana[i] > MAX_HORASLABOR) {
+					JOptionPane.showMessageDialog(null,
+							"No puede ingresa el número\nMáximo 40 horas laborado por " + (i + 1) + "° semana");
+				} else {
+					horasTotal += horasLaboralxSemana[i];
+				}
+			} while (horasLaboralxSemana[i] > MAX_HORASLABOR);
+		}	
+
+		//Ingresa valor hora laboral
+		salarioHora = Integer.parseInt(
+				JOptionPane.showInputDialog("Por favor ingresa valor hora")
+		);
+		 */
+		// Ingresa fecha de ingreso para bonficación
+		antiguedad = Integer.parseInt(JOptionPane.showInputDialog("Por favor ingresa cantidad años laborado por antiguedad"));
+
+		if (antiguedad >= 0 && antiguedad <= 3) {
+			JOptionPane.showMessageDialog(null, "Antiguedad es 0 años hasta 3 años");
+		} else {
+			if (antiguedad >= 4 && antiguedad <= 5) {
+				JOptionPane.showMessageDialog(null, "Antiguedad es desde 4 años hasta 5 años");
+			} else {
+				if (antiguedad >= 5 && antiguedad <= 10) {
+					JOptionPane.showMessageDialog(null, "Antiguedad es desde 5 años hasta menos 10 años");
+				} else {
+					JOptionPane.showMessageDialog(null, "Antiguedad es más 10 años");
+				}
+			}
+		}
+
+		/*
+		//Calcular salario x horas laborado durante mes
+		valorTotal = horasTotal * salarioHora;
+
+		//Muestra ventana contiene datos básicos y total horas laborales x mes
+		JOptionPane.showMessageDialog(null,
+				"Su identidad es " + identidad + ", Nombre: " + nombre
+				+ "\nedad: " + edad + ", genero: " + genero + ", estataura:" + estatura + "cm"
+				+ "\n" + horasTotal + " Horas Total laborado durante mes"
+				+ "\n $" + valorTotal + " Salario durante mes");
+		//+ "\nSu salario es " + (float) salario
+		 */
+	}
+}
+
+// Muestra ventana Horas laborales x semanas
+            //JOptionPane.showMessageDialog(null,semana + "° Semana" + horasLaboralxSemana[i] + " Horas Laborales");
