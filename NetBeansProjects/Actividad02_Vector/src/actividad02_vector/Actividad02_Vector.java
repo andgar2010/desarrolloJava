4c22f567b44a7bfdb86dced08f96d33a52f54f68
andga <afgarcia0479@misena.edu.co>
2017-08-11 00:34:26 -0500

    Add Comments

--- NetBeansProjects/Actividad02_Vector/src/actividad02_vector/Actividad02_Vector.java
+++ NetBeansProjects/Actividad02_Vector/src/actividad02_vector/Actividad02_Vector.java
@@ -19,28 +19,31 @@ public class Actividad02_Vector {
 	 * 
 	 */
 	public static void main(String[] args) {
-	
 		int posVector;
 
+		// Ingresa cantidad posición en vector
 		posVector = Integer.parseInt(
-				JOptionPane.showInputDialog("Por favor ingresa cantidad posición de vector"));
+			JOptionPane.showInputDialog("Por favor ingresa cantidad posición de vector"));
 		
+		//Crear vector
 		int vector[] = new int[posVector];
 		
+		// Ingresa los números por cada posición en vector
 		for (int i = 0; i < vector.length; i++) {
 			vector[i] = Integer.parseInt(
 				JOptionPane.showInputDialog(
 				"Ingresa los números por cada un espacio\nDigite el número en posición " + (i+1)
 				)
 			);
-		}
+		}//Fin ingresa los números por cada posición en vector
 		
+		// Muestra ventana los números en un vector
 		JOptionPane.showMessageDialog(null,
 			"Guardado los números en vector son");
 		
 		for (int i = 0; i < posVector; i++) {
 			System.out.println("|"+vector[i] + "|");
 			//JOptionPane.showMessageDialog(null, "|"+vector[i] + "|-");
-		}	
+		}//Fin Muestra ventana los números en un vector
 	}	
 }
