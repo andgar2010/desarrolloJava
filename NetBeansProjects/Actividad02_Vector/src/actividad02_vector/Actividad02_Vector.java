50f52772ceea3932af8a8eb61a924422eb44548a
andga <afgarcia0479@misena.edu.co>
2017-08-11 12:53:13 -0500

    Change mode to Method

--- NetBeansProjects/Actividad02_Vector/src/actividad02_vector/Actividad02_Vector.java
+++ NetBeansProjects/Actividad02_Vector/src/actividad02_vector/Actividad02_Vector.java
@@ -18,9 +18,9 @@ public class Actividad02_Vector {
 	 * @param args the command line arguments
 	 * 
 	 */
-	public static void main(String[] args) {
-		int posVector;
-		String temp = "";
+	public static void main(String[] args) 
+	{
+		int posVector;		
 
 		// Ingresa cantidad posición en vector
 		posVector = Integer.parseInt(
@@ -30,18 +30,37 @@ public class Actividad02_Vector {
 		int vector[] = new int[posVector];
 		
 		// Ingresa los números por cada posición en vector
-		for (int i = 0; i < vector.length; i++) {
+		llenar(vector);
+		 		
+		// Muestra ventana los números en un vector
+		imprimir(vector);
+		
+	}
+	
+	
+	public static void llenar(int vector[])
+	{
+		for (int i = 0; i < vector.length; i++) 
+		{
 			vector[i] = Integer.parseInt(
 				JOptionPane.showInputDialog(
-				"Ingresa los números por cada un espacio\nDigite el número en posición " + (i+1)
-				)
-			);
-			temp += " | " + vector[i];
-		}//Fin ingresa los números por cada posición en vector
+				"Ingresa los números por cada un espacio\nDigite el número en posición " + (i+1)));
+		}
 		
-		// Muestra ventana los números en un vector
+	}//Fin ingresa los números por cada posición en vector
+	
+	
+	public static void imprimir(int vector[])
+	{	
+		String temp = "";
+		
+		for (int i = 0; i < vector.length; i++) 
+		{			
+			temp += " | " + vector[i];
+		}
+		
 		JOptionPane.showMessageDialog(null,
 			"Guardado los números en vector son\n" + temp + " |");
-		//Fin Muestra ventana los números en un vector
-	}
+		
+	}//Fin Muestra ventana los números en un vector
 }
