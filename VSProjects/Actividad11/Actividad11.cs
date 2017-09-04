2848092e1e61cf554f4f953c9a25c1e67986c8e5
andga <afgarcia0479@misena.edu.co>
2017-09-04 11:03:45 -0500

    Completed Actividad 11 y 12 C#

---
+++ VSProjects/Actividad11/Actividad11.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad11
+{
+	public class Actividad11
+	{
+		static void Main(string[] args)
+		{
+			int numIngresa, opcionMenu, repInicio;
+
+			do
+			{
+				Console.WriteLine("\nSelecciona ejercicio:\n1. Edad\n2. Valor absoluto\n3. Cual posicion número");
+				opcionMenu = Int32.Parse(Console.ReadLine());
+
+				switch (opcionMenu)
+				{
+					case 1:
+						Console.WriteLine("\nIngresa su edad:");
+						int edad = Int32.Parse(Console.ReadLine());
+						Console.WriteLine((edad <= 3 && edad >= 0) ? ("Tu edad es bebé") : ((edad <= 12 && edad >= 4) ? ("Tu edad es Niño") : ((edad <= 17 && edad >= 13) ? ("Tu edad es Adolescente") : ((edad <= 100 && edad >= 18) ? ("Tu edad es Adulto") : ("Tu edad es fuera de limite???")))));
+						break;//Fin CASE 1 
+
+					case 2:
+						/* JAVA Ejemplo No 2. Operador ternario Calcule el valor absoluto de un número. Para ello utilizar dos variables: numero y valorAbsoluto. Repita con ciclo a su elección.*/
+						Console.WriteLine("\nIngresa el numero");
+						numIngresa = Int32.Parse(Console.ReadLine());
+						Console.WriteLine((numIngresa < 0) ? ("Valor absoluto es " + (numIngresa *= -1)) : ("Valor absoluto es " + numIngresa));
+						break;//Fin CASE 2 
+
+					case 3:
+						/*JAVA Ejemplo No 3. Operador ternario Lea un número e indique si es +, - , 0. Repita con ciclo a su elección.*/
+						Console.WriteLine("\nIngresa el numero");
+						numIngresa = Int32.Parse(Console.ReadLine());
+						Console.WriteLine((numIngresa == 0) ? ("Es número neturo") : ((numIngresa < 0) ? ("Es número negativo") : ("Es número postivo")));
+						break;//Fin CASE 3 
+				}//Fin menu	
+
+				Console.WriteLine("\nQue desea repetir desde menu?\nSelecciona\n1. Si\n2. No");
+				repInicio = Int32.Parse(Console.ReadLine());
+			}
+			while (repInicio == 1);
+		}//Fin Main
+	}
+}
---
+++ VSProjects/Actividad12/Actividad12.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad12
+{
+	class Actividad12
+	{
+		static void Main(string[] args)
+		{
+			int numInput, digitos, ultDig1_resultPot, ultDig2_resultPot, ultimos2Dig, cantDigResultPot, repInicio;
+			String numInputText, sResultPot, ultimos2DigitosResPot = "", msg = "";
+			long resultPot;
+
+				//Ingresa Usuario
+				Console.WriteLine("Ingrese un numero:");
+				numInput = Int32.Parse(Console.ReadLine());
+				numInputText = numInput + "";
+				digitos = numInputText.Length;
+
+				//CalcularPotencia
+				msg += "Base | Exponente | Resultado";
+				for (int i = 1; i <= numInput; i++)
+				{
+					resultPot = (long)Math.Pow(numInput, i);
+					sResultPot = "" + resultPot;
+					msg += "\n" + numInput + "    | " + i + "\t    | " + resultPot + " | ";
+
+					if (digitos == 1)
+					{
+						//Cantidad digitos de Resultado Potencia
+						cantDigResultPot = sResultPot.Length;
+
+						//Extraer ultima digito de Resultado Potencia "String" a variable ultDig_resultPot
+						ultDig1_resultPot = (int)char.GetNumericValue((sResultPot[cantDigResultPot - 1]));
+
+						//Comparativa inputUserNum == Ultima digito de resultado potencia
+						msg += (numInput == ultDig1_resultPot) ? (" Es automórfico") : (" No es automórfico");
+
+					}
+					else //digitos == 2
+					{
+						//Cantidad digitos de Resultado Potencia
+						cantDigResultPot = sResultPot.Length;
+
+						//Extraer ultima digito de Resultado Potencia "String" a variable int ultDig_resultPot
+						ultDig1_resultPot = (int)char.GetNumericValue(sResultPot[cantDigResultPot - 1]);
+
+						//Extraer pentiultima digito de Resultado Potencia "String" a variable int ultDig_resultPot
+						ultDig2_resultPot = (int)char.GetNumericValue(sResultPot[cantDigResultPot - 2]);
+
+						// Int ultDig1_resultPot y ultDig2_resultPot a String variable ultimos2DigitosResPot
+						ultimos2DigitosResPot = ultDig2_resultPot + "" + ultDig1_resultPot;
+
+						//String a int ultimos2Dig
+						ultimos2Dig = Int32.Parse(ultimos2DigitosResPot);
+
+						//Comparativa inputUserNum == Ultima digito de resultado potencia
+						msg += (numInput == ultimos2Dig) ? (" Es automórfico") : (" No es automórfico");
+					}
+
+					if (sResultPot.Length >= 19)
+					{
+						msg += "\n\nLo siento,\nno puede mostrar más " + i + " de exponente. Digitos esta fuera de limite";
+						break;
+					}
+
+				}//Fin CalcularPotencia - verificiarAutomorfico
+				Console.WriteLine(msg);
+
+			Console.WriteLine("Presione una tecla para continuar...");
+			Console.ReadKey(true);
+		}
+
+	}
+}
